9fac6d61db8cfa7fef688e4eb62d1ac25625864a
eylon <eylonnaamat@gmail.com>
2021-11-28 16:40:08 +0200

    updated mymat

--- my_mat.c
+++ my_mat.c
@@ -10,6 +10,35 @@ int min(int a, int b)
 }
 
 
+void shortestPath()
+{
+     for(int n = 0; n < 10; n++)
+    {
+        for (int m = 0; m < 10; m++)
+        {
+            for (int k = 0; k < 10; k++)
+            {
+                if((m == k) && (mat[m][k] == 0))
+                {
+                    mat[m][k] = 0;
+                }
+                else
+                {
+                    if(mat[m][k] == 0)
+                    {
+                        mat[m][k] =  mat[m][n] + mat[n][k];
+                    }
+                    else
+                    {
+                        int temp = mat[m][n] + mat[n][k];    
+                        mat[m][k] = min(mat[m][k], temp);
+                    }
+                }
+            }
+        }
+    }
+}
+
 void function1()
 {
     for(int i = 0; i < 10; i++)
@@ -18,26 +47,14 @@ void function1()
         {
             scanf("%d", &mat[i][j]);
         }
-    } 
+    }
+    shortestPath(); 
 }
 
 
 void function2(int i, int j)
 {
-    for(int n = 0; n < 10; n++)
-    {
-        for(int m = 0; m < 10; m++)
-        {
-            for(int k = 0; k < 10; k++)
-            {
-                if(mat[m][k] == 0 && m != k)
-                {
-                    mat[m][k] = mat[m][n] + mat[n][k];
-                }
-            }
-        }
-    }
-    if(mat[i][j] > 0)
+    if(mat[i][j] != 0)
     {
         printf("True");
     }
@@ -49,17 +66,6 @@ void function2(int i, int j)
 
 void function3(int i, int j)
 {
-    for(int n = 0; n < 10; n++)
-    {
-        for (int m = 0; m < 10; m++)
-        {
-            for (int k = 0; k < 10; k++)
-            {
-                int temp = mat[m][n] + mat[n][k];
-                mat[m][k] = min(mat[m][k], temp);
-            }
-        }
-    }
     if(mat[i][j] != 0)
     {
         printf("%d", mat[i][j]);
--- my_mat.h
+++ my_mat.h
@@ -1,3 +1,5 @@
 void function1();
 void function2(int i, int j);
 void function3(int i, int j);
+void shortestPath();
+int min(int a, int b);
