d8421e31a2fae0d32b3cc8564dc886ba9b5b2b96
eylon <eylonnaamat@gmail.com>
2021-11-28 12:05:04 +0200

    new my_mat

---
+++ my_mat.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include "my_mat.h"
+
+
+int mat [10][10];
+
+int min(int a, int b)
+{
+  return ((a < b) ? a : b);
+}
+
+
+void function1()
+{
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < 10; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
+    } 
+}
+
+
+void function2(int i, int j)
+{
+    for(int n = 0; n < 10; n++)
+    {
+        for(int m = 0; m < 10; m++)
+        {
+            for(int k = 0; k < 10; k++)
+            {
+                if(mat[m][k] == 0)
+                {
+                    mat[m][k] = mat[m][n] + mat[n][k];
+                }
+            }
+        }
+    }
+    if(mat[i][j] > 0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
+
+void function3(int i, int j)
+{
+    for(int n = 0; n < 10; n++)
+    {
+        for (int m = 0; m < 10; m++)
+        {
+            for (int k = 0; k < 10; k++)
+            {
+                int temp = mat[m][n] + mat[n][k];
+                mat[m][k] = min(mat[m][k], temp);
+            }
+        }
+    }
+    if(mat[i][j] != 0)
+    {
+        printf("%d", mat[i][j]);
+    }
+    else
+    {
+        printf("-1");
+    }
+}
+
+
+
+
---
+++ my_mat.h
@@ -0,0 +1,3 @@
+void function1();
+void function2(int i, int j);
+void function3(int i, int j);
